aa3cf24f360376bc7d3717b0041c11ed9d079d8b
Oleksandr Iaremii <ward.wsp@gmail.com>
2018-05-21 11:48:57 +0200

    initial classes and XML configuration file

---
+++ src/java_1_spring_core/TrackCoach.java
@@ -0,0 +1,13 @@
+package java_1_spring_core;
+
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+   
+
+}
