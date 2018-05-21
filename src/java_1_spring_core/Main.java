8a822f33f05a04e20f1c340b3f56175bfd6ae74b
Oleksandr Iaremii <ward.wsp@gmail.com>
2018-05-21 09:25:12 +0200

    initial repository

---
+++ src/java_1_spring_core/Main.java
@@ -0,0 +1,13 @@
+package java_1_spring_core;
+
+/**
+ *
+ * @author Oleksandr
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Course");
+    }
+
+}
