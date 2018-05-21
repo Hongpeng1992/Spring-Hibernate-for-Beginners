e87936aec9d7a89e911b2602bd1f4889c7129e6f
Oleksandr Iaremii <ward.wsp@gmail.com>
2018-05-21 14:33:31 +0200

    Read value from file

--- src/java_1_spring_core/CricketCoach.java
+++ src/java_1_spring_core/CricketCoach.java
@@ -1,22 +1,37 @@
 package java_1_spring_core;
 
-
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
+    private String email, team;
 
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
-        
+
     }
 
-    public void setFortuneService(FortuneService fortuneService) {
-         System.out.println("CricketCoach: inside setter method");
-        this.fortuneService = fortuneService;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
     
-    
-    
+
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method");
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
--- src/java_1_spring_core/SetterDemoApp.java
+++ src/java_1_spring_core/SetterDemoApp.java
@@ -7,9 +7,8 @@ public class SetterDemoApp {
     public static void main(String args[]) {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach theCricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
-        System.out.println(theCricketCoach.getDailyFortune());
-        System.out.println(theCricketCoach.getDailyWorkout());
+        CricketCoach theCricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
+        System.out.println(theCricketCoach.getEmail() + "\n" + theCricketCoach.getTeam());
     }
 
 }
