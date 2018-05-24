b67121d484aca8750f7b96f0a8319379a179febf
Oleksandr Iaremii <ward.wsp@gmail.com>
2018-05-24 18:01:05 +0200

    Add Config Annotation class(No XML file)

---
+++ src/SpringDemoAnnotations/springDemo/SportConfig.java
@@ -0,0 +1,10 @@
+package SpringDemoAnnotations.springDemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("SpringDemoAnnotations")
+public class SportConfig {
+
+}
--- src/SpringDemoAnnotations/springDemo/TennisCoach.java
+++ src/SpringDemoAnnotations/springDemo/TennisCoach.java
@@ -6,15 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
-    }
-    
-    
-    
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volleya";
